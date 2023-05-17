f6ccf254cb9150306e4ef162ca9e03627c2c3085
somen das <53004533+KTBsomen@users.noreply.github.com>
2023-05-17 11:09:51 +0530

    updated faster version

---
+++ ssheditV2.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+sudo apt install inotify-tools
+clear
+echo "ssh edit code locally by somen6562@gmail.com(webnima.in)"
+
+#read -p 'Host Ip: ' host
+#read -p 'Username: ' user
+#read -p 'name of keyfile with_full_path: ' key
+#read -p 'path of remote dir [with / at ending]: ' dir
+host=$(zenity --entry --width 500 --title "Host Ip" --text "Enter the Host Ip")
+user=$(zenity --entry --width 500 --title "Host Username" --text "Enter the username to connect")
+key=$(zenity --file-selection --title "Select Key file( *.pem )" --file-filter="*.pem")
+dir=$(zenity --entry --width 500 --title "Remote Directory" --text "Enter the path of remote directory with / at the ending")
+
+if [ ! -d "$host" ]; then
+
+mkdir $host
+  echo "$host directory is created and entering into it"
+ cd $host
+else
+newfile=$host"_"$(date "+%Y.%m.%d-%H.%M.%S")
+mkdir $newfile
+    echo "$newfile directory is created and entering into it"
+cd $newfile
+fi
+echo "Compressing files from remote this might take time if this taking too much of time to output anything then close the programm something is wrong "
+
+sudo ssh -i $key $user@$host "cd $dir;zip -r $newfile.zip ."
+echo "Downloading $newfile.zip if this taking too much of time to output anything then close the programm something is wrong "
+sudo scp -i $key -r $user@$host:$dir$newfile".zip" .
+sudo unzip $newfile".zip"
+sudo rm -r $newfile".zip"
+sudo ssh -i $key $user@$host "cd $dir;rm -r $newfile.zip"
+#read -p 'Type the sortcut command of the editor to open this directory :' 
+editor=$(zenity --entry --width 500 --title "favourite Editor" --text "Enter the command to open your fevourite text editor (without . )")
+clear
+echo "ssh edit code locally by somen6562@gmail.com(webnima.in)"
+sudo $editor . &
+targetDir="."
+inotifywait -mr $targetDir -e create -e moved_to -e modify --exclude '/\..+'|
+    while read path action file; do
+                 
+                 echo "$path It's a change there in  $file so updating to server"
+                 result=$(echo $path | sed "s@./@@")
+                 sudo scp -i $key $path$file $hostname@$host:$dir$result
+                 
+
+
+
+
+        # do something with the file
+    done
+
+
+
+
