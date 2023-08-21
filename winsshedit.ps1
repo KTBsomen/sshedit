74dc5cfbd3722316fcb4091e41f903e2f676e61a
somen das <53004533+KTBsomen@users.noreply.github.com>
2023-08-21 11:00:14 +0530

    Add files via upload

--- winsshedit.ps1
+++ winsshedit.ps1
@@ -1,17 +1,17 @@
 # Prompt for user input
-$host = Read-Host -Prompt "Host Ip"
+$hostip = Read-Host -Prompt "Host Ip"
 $user = Read-Host -Prompt "Host Username"
 $key = Read-Host -Prompt "Path of key file (*.pem)"
 $dir = Read-Host -Prompt "Remote Directory (with / at the end)"
 
 # Create the necessary directory
-if (-Not (Test-Path -Path "$host")) {
-    New-Item -ItemType Directory -Path $host | Out-Null
-    Write-Host "$host directory is created and entering into it"
-    Set-Location -Path $host
+if (-Not (Test-Path -Path "$hostip")) {
+    New-Item -ItemType Directory -Path $hostip | Out-Null
+    Write-Host "$hostip directory is created and entering into it"
+    Set-Location -Path $hostip
 }
 else {
-    $newfile = "$host" + "_" + (Get-Date -Format "yyyy.MM.dd-HH.mm.ss")
+    $newfile = "$hostip" + "_" + (Get-Date -Format "yyyy.MM.dd-HH.mm.ss")
     New-Item -ItemType Directory -Path $newfile | Out-Null
     Write-Host "$newfile directory is created and entering into it"
     Set-Location -Path $newfile
@@ -19,12 +19,12 @@ else {
 
 # Compress and download files from remote server
 Write-Host "Compressing files from remote. This might take time, please wait..."
-ssh -i $key $user@$host "cd $dir; zip -r $newfile.zip ."
+ssh -i $key $user@$hostip "cd $dir; zip -r $newfile.zip ."
 Write-Host "Downloading $newfile.zip. This might take time, please wait..."
-scp -i $key -r $user@$host:$dir$newfile.zip .
-unzip $newfile.zip
+scp -i $key -r "${user}@${host}:${dir}${newfile}.zip" .
+Expand-Archive -Path $newfile.zip -DestinationPath . -Force
 Remove-Item $newfile.zip
-ssh -i $key $user@$host "cd $dir; rm -r $newfile.zip"
+ssh -i $key $user@$hostip "cd $dir; rm -r $newfile.zip"
 
 # Prompt for editor command
 $editor = Read-Host -Prompt "Favorite Editor command (without .)"
@@ -40,7 +40,7 @@ while ($true) {
     if ($action -gt 0) {
         Write-Host "There is a change in the directory. Updating to the server..."
         $result = Get-Location | ForEach-Object { $_.Path.Substring(2) }
-        scp -i $key $result $user@$host:$dir -Recurse
+        scp -i $key "${result}" "${user}@${host}:${dir}" -Recurse
     }
     Start-Sleep -Seconds 2
 }
