7b6a9822a4f0d54bfc6f74f93bcc8d25a0d4f0fb
somen das <53004533+KTBsomen@users.noreply.github.com>
2024-01-18 20:50:44 +0530

    Update winsshedit.ps1

--- winsshedit.ps1
+++ winsshedit.ps1
@@ -1,9 +1,27 @@
 # Prompt for user input
-$hostip = Read-Host -Prompt "Host Ip"
-$user = Read-Host -Prompt "Host Username"
-$key = Read-Host -Prompt "Path of key file (*.pem)"
-$dir = Read-Host -Prompt "Remote Directory (with / at the end)"
+# File path
+$filePath = Read-Host -Prompt "config file path "
 
+# Read the content of the file
+$jsonContent = Get-Content -Path $filePath -Raw
+
+# Convert JSON content to PowerShell object
+$configData = $jsonContent | ConvertFrom-Json
+Write-Host "Choose a server:"
+for ($i = 0; $i -lt $configData.Count; $i++) {
+    Write-Host "$($i + 1). $($configData[$i].name)"
+}
+
+$serverChoice = Read-Host -Prompt "Enter the server number (1-$($configData.Count))"
+if ($serverChoice -le 1 -and $serverChoice -ge $configData.Count) {
+    Write-Host "Invalid server choice. Exiting script."
+    exit
+}
+$selectedServer = $configData[$serverChoice - 1]
+$global:hostip = $selectedServer.hostip
+$global:user = $selectedServer.username
+$global:key = $selectedServer.pemkeyfile
+$global:dir = $selectedServer.documentRoot
 # Create the necessary directory
 
 $newfile = "$hostip" + "_" + (Get-Date -Format "yyyy.MM.dd-HH.mm.ss")
@@ -19,24 +37,82 @@ ssh -i $key $user@$hostip "cd $dir; zip -r $newfile.zip ."
 Write-Host "Downloading $newfile.zip. This might take time, please wait..."
 scp -i $key -r "${user}@${hostip}:${dir}${newfile}.zip" .
 7z x "./${newfile}.zip"
-Remove-Item ./$newfile.zip
+#Remove-Item ./$newfile.zip
+Write-Host "Removing the zip file from remote"
+
 ssh -i $key $user@$hostip "cd $dir; rm -r $newfile.zip"
 
 # Prompt for editor command
 $editor = Read-Host -Prompt "Favorite Editor command (without .)"
 
 # Open the editor
-Start-Process -FilePath "$editor" -ArgumentList "$pwd" -Verb RunAs
+Start-Process -FilePath "$editor" -ArgumentList "$pwd" -NoNewWindow
+#use with above line for elevated permissions## -Verb RunAs
 
 # Watch for changes and update files on the remote server
 $targetDir = "$pwd"
+
 Write-Host "Watching for changes in the directory..."
-while ($true) {
-    $action = (Get-ChildItem $targetDir -Recurse -Force | Where-Object { $_.LastWriteTime -ne $_.CreationTime }).Count
-    if ($action -gt 0) {
-        Write-Host "There is a change in the directory. Updating to the server..."
-        $result = Get-Location | ForEach-Object { $_.Path.Substring(2) }
-        scp -i $key "${result}" "${user}@${host}:${dir}" -Recurse
+
+### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
+
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.Path = "$pwd"
+$watcher.Filter = "*.*"
+$watcher.IncludeSubdirectories = $true
+$watcher.EnableRaisingEvents = $true
+
+### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
+$action = {
+    $path = $Event.SourceEventArgs.FullPath 
+    $pathx = (Resolve-Path -Path $path -Relative).Replace(".\", "").Replace("\", "/")
+
+    $changeType = $Event.SourceEventArgs.ChangeType
+    $logline = "$(Get-Date), $changeType, $path"
+    #Write-Host "printing variables $key $user $hostip"
+
+    ###Add-content "C:\Users\somen\MOULD\sshedit\xx\log.txt" -value $logline
+    if (Test-Path -Path $path -PathType Leaf) {
+        #$scpCommand =  "scp -i `"$($key)`" `"$($path)`" `"$($user)@$($hostip):$($dir)$($pathx)`""
+
+        #Write-Host "SCP Command: $scpCommand"
+        
+        Write-Host "There is a change in the directory $pathx. Updating to the server..."
+        scp -i $key "$path" "${user}@${hostip}:${dir}${pathx}"
+        Write-Host "Done updating..."
+   
+
     }
-    Start-Sleep -Seconds 2
 }
+
+### delete action
+$delaction = {
+    $path = $Event.SourceEventArgs.FullPath
+    $changeType = $Event.SourceEventArgs.ChangeType
+    $logline = "$(Get-Date), $changeType, $path"
+    ###Add-content "$C:\Users\somen\MOULD\sshedit\xx\log.txt" -value $logline
+    if (Test-Path -Path $path -PathType Leaf) {
+        Write-Host "won't deleting file $logline"
+        ###ssh -i $key $user@$hostip "cd $dir; rm -r $newfile.zip"
+
+    }
+}
+
+### rename action
+$renameaction = {
+    $path = $Event.SourceEventArgs.FullPath
+    $changeType = $Event.SourceEventArgs.ChangeType
+    $logline = "$(Get-Date), $changeType, $path"
+    ###Add-content "C:\Users\somen\MOULD\sshedit\xx\log.txt" -value $logline
+    if (Test-Path -Path $path -PathType Leaf) {
+        Write-Host "$logline"
+    }
+}
+
+### DECIDE WHICH EVENTS SHOULD BE WATCHED
+Register-ObjectEvent $watcher "Created" -Action $action
+Register-ObjectEvent $watcher "Changed" -Action $action
+Register-ObjectEvent $watcher "Deleted" -Action $action
+Register-ObjectEvent $watcher "Renamed" -Action $action
+
+while ($true) { Start-Sleep 5 }
